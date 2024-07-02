abf64a440a8afa8d07d3ad8f3b1e9cd3ce851f83
Chhama Sharma <133862115+Konzaya@users.noreply.github.com>
2024-07-02 16:16:06 +0530

    Add files via upload

---
+++ quiz app/quiz-app/src/components/QuestionsData.js
@@ -0,0 +1,48 @@
+export const QuestionsData = [
+  {
+    question: "Who invented JavaScript?",
+    options: [
+      { text: "Douglas Crockford" },
+      { text: "Sheryl Sandberg" },
+      { text: "Brendan Eich" },
+    ],
+    answer: "Brendan Eich",
+  },
+  {
+    question: "Which one of these is a JavaScript package manager?",
+    options: [{ text: "Node.js" }, { text: "TypeScript" }, { text: "npm" }],
+    answer: "npm",
+  },
+  {
+    question: "Which tool can you use to ensure code quality?",
+    options: [
+      { text: "Angular" },
+      { text: "jQuery" },
+      { text: "RequireJS" },
+      { text: "ESLint" },
+    ],
+    answer: "ESLint",
+  },
+  {
+    question:
+      "When setState()  is called inside render() method, which of the following events takes place?",
+    options: [
+      { text: "duplicate key error" },
+      { text: "stackoverflow error" },
+      { text: "nothing happens" },
+      { text: "repetitive output happened on the screen" },
+    ],
+    answer: "stackoverflow error",
+  },
+  {
+    question:
+      "The function which is called to render HTML to a web page in react?",
+    options: [
+      { text: "ReactDOM_render()" },
+      { text: "render()" },
+      { text: "render_DOM()" },
+      { text: "DOM_Html()" },
+    ],
+    answer: "render()",
+  },
+];
